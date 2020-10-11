ea7a1a5945b2877b131de14213bc728242b73387
ssjisan <sakibfset13@gmail.com>
2020-10-11 19:09:26 +0600

    edited

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 /****************Connect to Cluster*************************/ 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.larxv.mongodb.net/${process.env.volunteerNetwork}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.larxv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("volunteerNetwork").collection("eventrego");
